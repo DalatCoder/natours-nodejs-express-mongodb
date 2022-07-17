0373eb6b9e4d1a8f644019e7a469b77a6783b371
DalatCoder <hieuntctk42@gmail.com>
2022-07-17 16:00:44 +0700

    document middleware

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = mongoose.Schema(
   {
@@ -7,6 +8,9 @@ const tourSchema = mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true
+    },
+    slug: {
+      type: String
     },
     duration: {
       type: Number,
@@ -67,6 +71,14 @@ const tourSchema = mongoose.Schema(
 
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
+});
+
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, {
+    lower: true
+  });
+
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
